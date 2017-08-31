4e08785ca60beb3b9e15539d80abb8af55db981d
romyephrem <romy.ephrem@isae.edu.lb>
2017-08-31 13:26:24 +0300

    class SACA,Radar,Controleur

---
+++ Documents/NetBeansProjects/ProjetAccovRomyServer/src/projetaccovromy/Server/Controleur.java
@@ -0,0 +1,15 @@
+package projetaccovromy.Server;
+/**
+ *
+ * @author Romy
+ */
+
+public class Controleur {
+    public String RenouvelerCoordonnees() {
+        return Math.random()*1000 + "-" + Math.random()*1000 + "-" + Math.random()*1000 + "-" + Math.random()*100 + "-" + Math.random()*20000;
+    }
+    
+    public String RenouvelerCoordonnees(int vitx, int vity, int vitz, int cap, int alt) {
+        return vitx + "-" + vity + "-" + vitz + "-" + cap + "-" + alt;
+    }
+}
